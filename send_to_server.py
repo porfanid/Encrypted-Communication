7b958fe4f2cbbbc501664a9fe69a7d41b774a7c1
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2023-08-11 17:10:18 +0300

    Added code to access the firebase account

---
+++ send_to_server.py
@@ -0,0 +1,24 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+class Database():
+
+    def __init__(self):
+        cred = credentials.Certificate("./personal-site-key.json")
+
+        firebase_admin.initialize_app(cred)
+
+        # Initialize Firestore
+        self.db = firestore.client()
+
+    def register_client(self, client_key, client_email):
+        # Create a new document in a collection named "entries"
+        collection_ref = self.db.collection('clients')
+        new_doc_ref = collection_ref.add({
+            'key': client_key,
+            'email': client_email,
+            'timestamp': firebase_admin.firestore.SERVER_TIMESTAMP  # Optional timestamp
+        })
+
+        print("Text entry stored in Firestore with ID:", new_doc_ref)
+        return True
