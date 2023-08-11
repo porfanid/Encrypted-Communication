fc082a3adf5fdeb094bfcf50b911162a9d30097d
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2023-08-11 17:10:39 +0300

    when registering a new key, store it to the firebase too

--- gen.py
+++ gen.py
@@ -1,10 +1,15 @@
 import gnupg
 import os
 
+
 class Encrypted_Communication():
-    def __init__(self):
+    def __init__(self, database):
         self.gpg = gnupg.GPG(gnupghome=os.path.expanduser("~/.gnupg"))  
+    
     def generate_key(self, email):
+        if os.path.exists("./keys/personal.asc"):
+            return str(open("./keys/personal.asc", "r").read())
+
         input_data = self.gpg.gen_key_input(
             name_email=email)
         key = self.gpg.gen_key(input_data).fingerprint
@@ -13,9 +18,13 @@ class Encrypted_Communication():
 
         with open("keys/personal.asc", "w") as public_key_file:
             public_key_file.write(public_key)
+        database.register_client(str(public_key), email)
+        return str(public_key)
+
     def encrypt_message(self,message, email):
         encrypted_data = self.gpg.encrypt(message, [email])
         return str(encrypted_data)
+
     def decrypt_message(self, message):
         decrypted_data = self.gpg.decrypt(
             message)
