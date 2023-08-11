13b265587ffdb6d038e92d5777f3c73b2efd0229
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2023-08-11 19:15:08 +0300

    Added code to encrypt the message and store it to the database

--- send_to_server.py
+++ send_to_server.py
@@ -3,14 +3,15 @@ from firebase_admin import credentials, firestore
 
 class Database():
 
-    def __init__(self):
+    def __init__(self, email):
         cred = credentials.Certificate("./personal-site-key.json")
 
         firebase_admin.initialize_app(cred)
 
         # Initialize Firestore
         self.db = firestore.client()
-
+        self.email=email
+    
     def register_client(self, client_key, client_email):
         # Create a new document in a collection named "entries"
         collection_ref = self.db.collection('clients')
@@ -24,3 +25,27 @@ class Database():
 
         print("Text entry stored in Firestore with ID:", new_doc_ref)
         return True
+    
+    def send_message(self, client_email, message):
+        collection_ref = self.db.collection('messages')
+        other_user = collection_ref.document(client_email)
+        new_message_for_other_user = {
+            'message': message,
+            'from': self.email
+        }
+
+        
+        # Check if the other user's document exists
+        if other_user.get().exists:
+            # Retrieve the existing list of messages for the other user
+            other_user_data = other_user.get().to_dict()
+            other_user_messages = other_user_data.get('messages', [])
+            
+            # Add the new message to the other user's list
+            other_user_messages.append(new_message_for_other_user)
+            
+            # Update the other user's document with the new message list
+            other_user.set({'messages': other_user_messages}, merge=True)
+        else:
+            # Create the other user's document with the new message
+            other_user.set({'messages': [new_message_for_other_user]})
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from send_to_server import Database
-test_database=Database()
+test_database=Database("test@orfanidis.net.gr")
 from gen import Encrypted_Communication
 client=Encrypted_Communication(test_database)
 key=client.generate_key("test@orfanidis.net.gr")
-print(client.encrypt_message("hello", key))
+message = client.encrypt_message("hello", key)
+test_database.send_message("pavlos@orfanidis.net.gr", message)
